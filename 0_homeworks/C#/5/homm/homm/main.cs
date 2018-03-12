52ce84637581d4f877f96d3d74934ed8101e3be9
anonymous <user@step.itstep.rv.ua>
2018-03-12 20:51:22 +0200

    homm

---
+++ 0_homeworks/C#/5/homm/homm/main.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homm{
+	struct Coord {
+		public short x, y;
+		public Coord(System.SByte X, System.SByte Y) { x = X; y = Y; }
+	}
+    class Hero{
+		public byte atk, def, moral, luck;
+    }
+
+	class StackUnit {
+		public GameTitle title { private set; get; }
+		public byte atk, def, dmgMin, dmgMax, speed, hp;
+		byte atkRange;
+		bool isFlying;
+
+		short number;
+		Coord pos;
+
+		Hero hero;
+
+		bool MoveAttack() {
+			return true;
+		}
+		bool Move() {
+			return true;
+		}
+		bool Step(GameTitle title) {
+			return true;
+		}
+
+		/*bool Attack(ref StackUnit enemy) {
+			ushort dmg = 0;
+			enemy.GetDMG(dmg, (byte)(atk + hero.atk));
+			return true;
+		}*/
+
+		void GetDMG(ushort dmg, byte heroAtk) {
+
+		}
+	}
+
+	class Army {
+		Hero hero;
+		StackUnit[] units; 
+	}
+
+	class Fraction {
+
+	}
+
+	class Game {
+		BattleMap map;
+		//Army p1, p2;
+
+		public Game() {
+			map = new BattleMap();
+		}
+
+		public void Play() {
+			PrintCurrState();
+		}
+
+		void PrintCurrState() {
+			map.Print(new Coord(5,5));
+		}
+	}
+
+	class GameSingletone {
+		static public Game game { get;  set; }
+
+		static GameSingletone() {
+			game = new Game();
+		}
+	}
+
+
+    class CMain{
+        static void Main(string[] args){
+			GameSingletone.game.Play();
+		}
+    }
+}
---
+++ 0_homeworks/C#/5/homm/homm/map.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace homm {
+	class GameTitle {
+		public ConsoleColor fore, back;
+		public char symbol;
+		public StackUnit currUnit;
+
+		public GameTitle() {
+			fore = ConsoleColor.Gray;
+			back = ConsoleColor.Black;
+			symbol = ' ';
+			currUnit = null;
+		}
+
+		public void Print() {
+			if (Console.ForegroundColor != fore)
+				Console.ForegroundColor = fore;
+			if (Console.BackgroundColor != back)
+				Console.BackgroundColor = back;
+			if (currUnit == null)
+				Console.Write(symbol);
+			else
+				Console.Write(currUnit.title.symbol);
+		}
+	}
+
+	class BattleMap {
+		GameTitle[,] map;
+
+		void GenerateLandscape() {
+			for (byte i = 0; i < map.GetLength(0); ++i)
+				for (byte j = 0; j < map.GetLength(1); ++j) {
+					map[i, j].symbol = '-';
+					//map[i, j].back = ConsoleColor.DarkYellow;
+				}
+		}
+
+		public BattleMap() {
+			map = new GameTitle[11, 15];
+			for (byte i = 0; i < map.GetLength(0); ++i)
+				for (byte j = 0; j < map.GetLength(1); ++j)
+					map[i, j] = new GameTitle();
+			GenerateLandscape();
+		}
+
+		void RemoveUnit(Coord pos) {
+			map[pos.y, pos.x].currUnit = null;
+		}
+
+		void SetUnit(Coord pos, ref StackUnit unit) {
+			map[pos.y, pos.x].currUnit = unit;
+		}
+
+		public void Print(Coord start) {
+			Console.CursorLeft = start.x;
+			Console.CursorTop = start.y;
+			for (byte i = 0; i < map.GetLength(0); ++i) {
+				for (byte j = 0; j < map.GetLength(1); ++j) {
+					Console.Write(' ');
+					map[i, j].Print();
+					Console.Write(' ');
+				}
+				start.y += 2;
+				Console.CursorLeft = start.x;
+				Console.CursorTop  = start.y;
+			}
+		}
+	}
+}
