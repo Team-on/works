863173e6e41d794d24ae5e93f4a359244fd66777
Team-on <timonsol13@gmail.com>
2018-03-29 16:08:52 +0300

    xmlParser

---
+++ 0_homeworks/C#/9/XMLDirParser/Support.cs
@@ -0,0 +1,20 @@
+﻿namespace TeamCommander {
+	namespace Support {
+		class Coord {
+			public short x, y;
+
+			public Coord() {
+				x = y = 0;
+			}
+
+			public Coord(short X, short Y) {
+				x = X;
+				y = Y;
+			}
+			public Coord(int X, int Y) {
+				x = (short)X;
+				y = (short)Y;
+			}
+		}
+	}
+}
