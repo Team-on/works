1adb37824ab960bff9ec10849ccada98c2367eea
Team-on <timonsol13@gmail.com>
2018-08-21 19:29:10 +0300

    Done ADO.NET 1

--- 0_homeworks/C#/4 adonet/1/TestADONET/Program.cs
+++ 0_homeworks/C#/4 adonet/1/TestADONET/Program.cs
@@ -8,8 +8,14 @@ using System.Data.SqlClient;
 
 namespace TestADONET {
 	class Program {
-		static void Main(string[] args) {
+		static Random random = new Random();
 
+		static void Main(string[] args) {
+			CreateUserDB();
+			ReadSchoolDB("School", "NVK666Pupil");
+		}
+
+		static void CreateUserDB(){
 			SqlConnection con = new SqlConnection(@"Server=(localdb)\mssqllocaldb; Database=master; Integrated Security=True");
 
 			try {
@@ -19,8 +25,7 @@ namespace TestADONET {
 
 
 				SqlCommand com = new SqlCommand(
-					"IF EXISTS(select * from sys.databases where name='Users') " +
-					"DROP DATABASE Users " + 
+					"IF NOT EXISTS(select * from sys.databases where name='Users') " +
 					"CREATE DATABASE Users ",
 					con
 				);
@@ -29,40 +34,79 @@ namespace TestADONET {
 				Console.WriteLine("Created\n");
 
 				Console.WriteLine("Reopen START");
-				con.Close();
-				con = new SqlConnection(@"Server=(localdb)\mssqllocaldb; Database=Users; Integrated Security=True");
-				con.Open();
-				com.Connection = con;
+				con.ChangeDatabase("Users");
 				Console.WriteLine("Reopen END\n");
 
-				com.CommandText = "CREATE TABLE User( " +
+				com.CommandText = "IF NOT EXISTS(select * from sys.tables where name='_User') " +
+					"CREATE TABLE _User ( " +
 					"IdUser TINYINT IDENTITY PRIMARY KEY, " +
+					"FirstName NVARCHAR(32), " +
+					"SecondtName NVARCHAR(32), " +
+					"ThirdName NVARCHAR(32), " +
 					"Login NVARCHAR(16) NOT NULL, " +
-					"Password NVARCHAR(32) NOT NULL" +
+					"Password NVARCHAR(32) NOT NULL " +
 					")";
 				Console.WriteLine("Creating table");
 				com.ExecuteNonQuery();
 				Console.WriteLine("Created table\n");
+
+				
+				Console.WriteLine("Inserting values");
+				string[] passToCreate = new string[] {
+					"qwerty",
+					"123456",
+					"<juZjqSQjuj{fpz]y",
+					"UfknhfyPFD:LBPkbdf'Nfynjce",
+					"Lf<eltnNfr!1",
+				};
+				for(byte i = 0; i < passToCreate.Length; ++i)
+					InsertValue($"Dude{i}", $"Dude{i}", $"Dude{i}", $"Dude{i}", passToCreate[i]);
+				Console.WriteLine("Values inserted!\n");
+
+				ReadSchoolDB("Users", "_User");
+
+				//
+				void InsertValue(string fname, string sname, string tname, string login, string pass){
+					string key = "YtrhjvfynDsl<j;tymrb";
+					StringBuilder encryptPass = new StringBuilder(pass.Length + 3, pass.Length + 3);
+					//StringBuilder backTest = new StringBuilder(pass.Length, pass.Length);
+
+					//Шифрування через XOR
+					// Додаю 1 випадковий символ на початок і 2 в кінець
+					encryptPass.Append((char) random.Next(2, 130));
+					for(byte i = 0; i < pass.Length; i++) {
+						encryptPass.Append((char) (pass[i] ^ key[i % key.Length]));
+						//backTest.Append((char) (encryptPass[i + 1] ^ key[i % key.Length]));
+					}
+					encryptPass.Append((char)random.Next(0, 255));
+					encryptPass.Append((char) random.Next(130, 200));
+
+					//Console.WriteLine($"{pass:20}\n{encryptPass:20}\n{backTest:20}\n\n");
+
+					com.CommandText = "INSERT INTO _User " +
+						"(FirstName, SecondtName, ThirdName, Login, Password)" +
+						"VALUES " +
+						$"(N'{fname}', N'{sname}', N'{tname}', N'{login}', N'{encryptPass}') ";
+					com.ExecuteNonQuery();
+				}
 			}
-			catch (System.Exception ex) {
+			catch(System.Exception ex) {
 				Console.WriteLine($"ERROR: {ex}");
 			}
 			finally {
-				Console.WriteLine("Closing");
-				if (con.State == ConnectionState.Open) {
+				Console.WriteLine("\nClosing");
+				if(con.State == ConnectionState.Open) {
 					con.Close();
 					Console.WriteLine("Closed\n");
 				}
 				else
 					Console.WriteLine("Warning: Already closed\n");
 			}
-
-			//ReadSchoolDB();
 		}
 
-		static void ReadSchoolDB() {
+		static void ReadSchoolDB(string database, string table) {
 			Console.WindowWidth = 150;
-			SqlConnection connection = new SqlConnection(@"Server=(localdb)\mssqllocaldb; Database=School; Integrated Security=True");
+			SqlConnection connection = new SqlConnection($@"Server=(localdb)\mssqllocaldb; Database={database}; Integrated Security=True");
 			try {
 				Console.WriteLine("Opening");
 				connection.Open();
@@ -73,10 +117,10 @@ namespace TestADONET {
 					Console.WriteLine($"{i.Name,-32}: {i.GetValue(connection)}");
 				Console.WriteLine('\n');
 
-				SqlCommand command = new SqlCommand("select count(*) from NVK666Pupil", connection);
+				SqlCommand command = new SqlCommand($"select count(*) from {table}", connection);
 				Console.WriteLine($"Count (*): {command.ExecuteScalar()}");
 
-				command.CommandText = "select * from NVK666Pupil";
+				command.CommandText = $"select * from {table}";
 				using (SqlDataReader reader = command.ExecuteReader()) {
 					if (reader.HasRows) {
 						Console.WriteLine($"FieldCount: {reader.FieldCount}");
