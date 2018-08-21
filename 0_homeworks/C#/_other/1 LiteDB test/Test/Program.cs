9092c6dbd529d06be41a175d4665945ebf258624
Team-on <timonsol13@gmail.com>
2018-08-21 16:09:48 +0300

    LiteDB test file download/upload

--- 0_homeworks/C#/_other/1 LiteDB test/Test/Program.cs
+++ 0_homeworks/C#/_other/1 LiteDB test/Test/Program.cs
@@ -4,10 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.IO;
+
 using LiteDB;
 using LiteDB.Shell;
 
 namespace Test {
+	///http://www.litedb.org/
+
 	public class Company {
 		public Guid Id { get; set; }
 		public string Name { get; set; }
@@ -24,20 +28,17 @@ namespace Test {
 				var col = db.GetCollection<Company>("companies");
 
 				var microsoft = new Company { Name = "Microsoft" };
-				microsoft.Users = new List<User> { new User { Name = "Bill Gates" } };
-
+				microsoft.Users = new List<User> { new User { Name = "Bill Gates" }, new User() { Name = "MS user" } };
 				col.Insert(microsoft);
+
+				var google = new Company { Name = "Google" };
+				google.Users = new List<User> { new User { Name = "Larry Page" }, new User() { Name = "Google user" } };
+				col.Insert(google);
 
 				microsoft.Name = "Microsoft Inc.";
 				col.Update(microsoft);
 
-
-				var google = new Company { Name = "Google" };
-				google.Users = new List<User> { new User { Name = "Larry Page" } };
-				col.Insert(google);
-
-				var result = col.FindAll();
-				foreach(Company c in result) {
+				foreach(Company c in col.FindAll()) {
 					Console.WriteLine(c.Name);
 					foreach(User u in c.Users)
 						Console.WriteLine(u.Name);
@@ -45,18 +46,22 @@ namespace Test {
 				}
 
 				col.EnsureIndex(x => x.Name);
-
 				col.Delete(x => x.Name.Equals("Google"));
 
 				Console.WriteLine("После удаления Google");
-				result = col.FindAll();
-				foreach(Company c in result) {
+				foreach(Company c in col.FindAll()) {
 					Console.WriteLine(c.Name);
 					foreach(User u in c.Users)
 						Console.WriteLine(u.Name);
 					Console.WriteLine();
 				}
+
+
+				db.FileStorage.Upload("image.jpg", @"D:\home\Arthasdeathknight.jpg");
+				db.FileStorage.FindById("image.jpg").SaveAs(@"D:\ArthasdeathknightFromDB.jpg");
 			}
+			
+
 		}
 	}
 }
