0c029c72b5182d0d8b91cb2cb9c156c2c6b10272
Team-on <timonsol13@gmail.com>
2018-08-21 03:33:59 +0300

    Change ext

---
+++ 0_homeworks/C++/basic/1_base/Пара_1(cin,cout)/ДЗ.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+int main()
+{
+setlocale(0,"rus");
+
+/* �������� 1
+cout << "Even a well-lit place can hide salvation\nA map to a one-man maze that never sees the sun\nWhere the lost are the heroes\nAnd the thieves are left to drown\nBut everyone knows by now\nFairy tales are not found\nThey\'re written in the walls" << endl;
+cout <<"\n\t\t\t by Starset";
+*/
+
+/* �������� 2
+cout <<"������ "<<endl<<"\t������� "<<endl<<"\t\t������ "<<endl<<"\t\t\t�����, "<<endl<<"\t\t\t\t��� "<<endl<<"\t\t\t\t\t����� "<<endl<<"\t\t\t\t\t\t�����.";
+*/
+
+cout <<"\t\t\t������ ������"<<endl;
+cout <<"\t\t\t������: ��������������� ������"<<endl;
+cout <<"\t\t\t���: �������"<<endl;
+cout <<"\t\t\t��������: ���"<<endl;
+cout <<"\t\t\t�����������: ����\n"<<endl;
+cout <<"\t\t\t���������� ����������:"<<endl;
+cout <<"\t\t\t+380991582739"<<endl;
+cout <<"\t\t\t\t����, �������, ����";
+
+return 0;
+}
