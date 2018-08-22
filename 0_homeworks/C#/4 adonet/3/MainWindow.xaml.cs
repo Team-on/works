822f05ffa3f41168f9297433c7613b9b55b9bac3
Team-on <timonsol13@gmail.com>
2018-08-22 20:50:22 +0300

    Phonebook

---
+++ 0_homeworks/C#/4 adonet/3/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using EzDBAccess;
+
+namespace Phonebook {
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window {
+		EzDBList<PhoneBook> phonebook;
+		PhoneBook currPhone;
+
+		public MainWindow() {
+			InitializeComponent();
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e) {
+			phonebook = new EzDBList<PhoneBook>("System.Data.SqlClient", @"Server=(localdb)\mssqllocaldb; Database=master; Integrated Security=True", false);
+			currPhone = new PhoneBook();
+			dataGrid.ItemsSource = phonebook;
+		}
+
+		private void AddBtn_Click(object sender, RoutedEventArgs e) {
+			phonebook.Add(new PhoneBook("123", "c", "s", "n", "h"));
+			dataGrid.Items.Refresh();
+		}
+
+		private void RemoveBtn_Click(object sender, RoutedEventArgs e) {
+
+		}
+	}
+}
---
+++ 0_homeworks/C#/4 adonet/3/PhoneBook.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phonebook {
+	class PhoneBook {
+		public int Id {get; set;}
+		public string Phone {get; set;}
+		public string AdressCity {get; set;}
+		public string AdressStreet { get; set;}
+		public string AdressStreetNum { get; set;}
+		public string AdressHome { get; set;}
+
+		public PhoneBook() {}
+
+		public PhoneBook(string phone, string adressCity, string adressStreet, string adressStreetNum, string adressHome) {
+			Phone = phone;
+			AdressCity = adressCity;
+			AdressStreet = adressStreet;
+			AdressStreetNum = adressStreetNum;
+			AdressHome = adressHome;
+		}
+	}
+}
