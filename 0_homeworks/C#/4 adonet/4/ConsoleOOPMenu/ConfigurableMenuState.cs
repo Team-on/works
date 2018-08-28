02842c29438283392414301652a46ad7d8626070
Team-on <timonsol13@gmail.com>
2018-08-28 20:48:24 +0300

    ConfMenuStaet

---
+++ 0_homeworks/C#/4 adonet/4/ConsoleOOPMenu/ConfigurableMenuState.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleOOPMenu {
+	public class ConfigurableMenuState : MenuState {
+		private Dictionary<int, MenuItem> menus = new Dictionary<int, MenuItem>();
+		private Dictionary<int, Func<IState>> transitions = new Dictionary<int, Func<IState>>();
+
+		protected override Dictionary<int, MenuItem> Menus => menus;
+
+		protected override IState NextState(KeyValuePair<int, MenuItem> selectedMenu) {
+			return transitions[selectedMenu.Key]();
+		}
+
+		public void AddMenuItem(int id, MenuItem menu, Func<IState> nextState) {
+			menus.Add(id, menu);
+			transitions.Add(id, nextState);
+		}
+	}
+}
--- 0_homeworks/C#/4 adonet/4/GraphicFilesDB/Program.cs
+++ 0_homeworks/C#/4 adonet/4/GraphicFilesDB/Program.cs
@@ -37,7 +37,31 @@ namespace GraphicFilesDB {
 
 	class Program {
 		static void Main(string[] args) {
-			IState startState = new TestMenuState();
+			//-------------------------------- 1 --------------------------------
+			//IState startState = new TestMenuState();
+			//while(startState != null)
+			//	startState = startState.RunState();
+
+			var menuState = new ConfigurableMenuState();
+			menuState.AddMenuItem(1, new MenuItem() { Text = "Print 10" }, () => {
+				Console.WriteLine(10);
+				return menuState;
+			});
+			menuState.AddMenuItem(2, new MenuItem() { Text = "Print files" }, () => {
+				FileFinderSettings settings = new FileFinderSettings();
+				settings.ReadFromXml(@".\settings.xml");
+
+				FileFinder fileFinder = new FileFinder(settings);
+				fileFinder.Find();
+
+				foreach(var file in fileFinder.Files)
+					Console.WriteLine(file.FullName);
+
+				return menuState;
+			});
+			menuState.AddMenuItem(3, new MenuItem() { Text = "Exit" }, () => null);
+
+			IState startState = menuState;
 			while(startState != null)
 				startState = startState.RunState();
 		}
