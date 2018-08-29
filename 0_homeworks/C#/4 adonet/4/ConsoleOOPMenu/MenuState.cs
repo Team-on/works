36e648f2fdc1b2a9f4516c5f7e8bb98d95ce62c1
Team-on <timonsol13@gmail.com>
2018-08-29 18:58:49 +0300

    Color in Menu item

--- 0_homeworks/C#/4 adonet/4/ConsoleOOPMenu/MenuState.cs
+++ 0_homeworks/C#/4 adonet/4/ConsoleOOPMenu/MenuState.cs
@@ -5,13 +5,38 @@ namespace ConsoleOOPMenu{
 	public abstract class MenuState : IState {
 		protected abstract Dictionary<int, MenuItem> Menus { get; }
 
+		protected MenuItem PreReadMessage;
+		protected MenuItem ErrorNonexist;
+		protected MenuItem ErrorNonnumber;
+
+		public MenuState() {
+			PreReadMessage = new MenuItem() {
+				Text = "\nPlease, select option:\n"
+			};
+
+			ErrorNonexist = new MenuItem() {
+				Text = "Selected item notexists.\n",
+				PreText = "ERROR:",
+				PreTextColor = ConsoleColor.Red,
+			};
+
+			ErrorNonnumber = new MenuItem() {
+				Text = "Selected item not a number.\n",
+				PreText = "ERROR:",
+				PreTextColor = ConsoleColor.Red,
+			};
+		}
+
 		protected virtual void ShowMenu() {
-			foreach(var m in Menus)
-				Console.WriteLine($"[{m.Key}] - {m.Value.Text}");
+			foreach(var m in Menus) {
+				Console.Write($"[{m.Key}] - ");
+				m.Value.Print();
+				Console.WriteLine();
+			}
 		}
 
 		protected virtual KeyValuePair<int, MenuItem> ReadOption() {
-			Console.WriteLine("\nPlease, select option:");
+			PreReadMessage.Print();
 			ShowMenu();
 
 			var str = Console.ReadLine();
@@ -19,13 +44,13 @@ namespace ConsoleOOPMenu{
 
 			if(byte.TryParse(str, out byte answerId)) {
 				if(!Menus.ContainsKey(answerId)) {
-					Console.WriteLine("ERROR: Selected item notexists.");
+					ErrorNonexist.Print();
 					return ReadOption();
 				}
 				return new KeyValuePair<int, MenuItem>(answerId, Menus[answerId]);
 			}
 			else {
-				Console.WriteLine("ERROR: Selected item not a number.");
+				ErrorNonnumber.Print();
 				return ReadOption();
 			}
 		}
