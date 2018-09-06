fa50138379a46dd9f706ba5fb9cdc03374483845
Team-on <timonsol13@gmail.com>
2018-09-06 19:12:34 +0300

    Done Fake3D example

--- 3_dll/[C#] Fake3D/example/ex1/MainWindow.xaml.cs
+++ 3_dll/[C#] Fake3D/example/ex1/MainWindow.xaml.cs
@@ -20,66 +20,10 @@ namespace ex1 {
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
 	public partial class MainWindow : Window {
-		List<Line3D> lines3D = new List<Line3D> {
-			/*
-			//Піраміда
-			new Line3D(-100, -100, -100, -100,  100, -100),
-			new Line3D(-100, -100, -100,  100, -100, -100),
-
-			new Line3D(100, 100, -100, -100,  100, -100),
-			new Line3D(100, 100, -100,  100, -100, -100),
-
-			new Line3D(0, 0, 100,  100,  100, -100),
-			new Line3D(0, 0, 100, -100,  100, -100),
-			new Line3D(0, 0, 100,  100, -100, -100),
-			new Line3D(0, 0, 100, -100, -100, -100),
-			*/
-
-			/*
-			//Куб
-			new Line3D(-100, -100, -100, -100,  100, -100),
-			new Line3D(-100, -100, -100,  100, -100, -100),
-			new Line3D(100, 100, -100, -100,  100, -100),
-			new Line3D(100, 100, -100,  100, -100, -100),
-
-			new Line3D(-100, -100, 100, -100,  100, 100),
-			new Line3D(-100, -100, 100,  100, -100, 100),
-			new Line3D(100, 100, 100, -100,  100, 100),
-			new Line3D(100, 100, 100,  100, -100, 100),
-
-			new Line3D(-100, -100, -100, -100, -100, 100),
-			new Line3D(100, -100, -100, 100, -100, 100),
-			new Line3D(-100, 100, -100, -100, 100, 100),
-			new Line3D(100, 100, -100, 100,  100, 100),
-			*/
-		};
+		List<Line3D> lines3D = new List<Line3D>();
 
 		public MainWindow() {
 			InitializeComponent();
-
-			var body = new Cube3D(-40, -50, -25, 40, 70, 25);
-			lines3D.AddRange(body.GetLines());
-
-			var head = new Cube3D(-20, -50, -25, 20, -90, 25);
-			lines3D.AddRange(head.GetLines());
-
-			var leye = new Cube3D(-15, -80, -25, -2, -75, -25);
-			lines3D.AddRange(leye.GetLines());
-
-			var reye = new Cube3D(2, -80, -25, 15, -75, -25);
-			lines3D.AddRange(reye.GetLines());
-
-			var larm = new Cube3D(40, -50, -12, 60, 40, 12);
-			lines3D.AddRange(larm.GetLines());
-
-			var rarm = new Cube3D(-40, -50, -12, -60, 40, 12);
-			lines3D.AddRange(rarm.GetLines());
-
-			var lleg = new Cube3D(-30, 70, -14, -10, 170, 14);
-			lines3D.AddRange(lleg.GetLines());
-
-			var rleg = new Cube3D(10, 70, -14, 30, 170, 14);
-			lines3D.AddRange(rleg.GetLines());
 		}
 
 		private void sliderz_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
@@ -107,11 +51,99 @@ namespace ex1 {
 		}
 
 		private void Window_Loaded(object sender, RoutedEventArgs e) {
+			//AddLines();
+			PiramideMenuItem_Click(null, null);
+		}
+
+		void AddLines() {
 			Line3D.CalcValues(out double p1, out double q1, out double r1, out double p2, out double q2, out double r2, sliderx.Value, slidery.Value, sliderz.Value);
 			foreach(var line in lines3D) {
 				canvas.Children.Add(line.Get2DLine());
 				line.Recalc2DLine(p1, q1, r1, p2, q2, r2);
 			}
+		}
+
+		void ClearLines() {
+			lines3D.Clear();
+			canvas.Children.Clear();
+		}
+
+		private void PiramideMenuItem_Click(object sender, RoutedEventArgs e) {
+			PiramideMenuItem.IsChecked = true;
+			CubeMenuItem.IsChecked = HumanMenuItem.IsChecked = false;
+
+			ClearLines();
+			lines3D.AddRange(new[] {
+				new Line3D(-100, -100, -100, -100,  100, -100),
+				new Line3D(-100, -100, -100,  100, -100, -100),
+
+				new Line3D(100, 100, -100, -100,  100, -100),
+				new Line3D(100, 100, -100,  100, -100, -100),
+
+				new Line3D(0, 0, 100,  100,  100, -100),
+				new Line3D(0, 0, 100, -100,  100, -100),
+				new Line3D(0, 0, 100,  100, -100, -100),
+				new Line3D(0, 0, 100, -100, -100, -100),
+			});
+
+			AddLines();
+		}
+
+		private void CubeMenuItem_Click(object sender, RoutedEventArgs e) {
+			CubeMenuItem.IsChecked = true;
+			HumanMenuItem.IsChecked = PiramideMenuItem.IsChecked = false;
+
+			ClearLines();
+			lines3D.AddRange(new[] {
+				new Line3D(-100, -100, -100, -100, 100, -100),
+				new Line3D(-100, -100, -100, 100, -100, -100),
+				new Line3D(100, 100, -100, -100, 100, -100),
+				new Line3D(100, 100, -100, 100, -100, -100),
+
+				new Line3D(-100, -100, 100, -100, 100, 100),
+				new Line3D(-100, -100, 100, 100, -100, 100),
+				new Line3D(100, 100, 100, -100, 100, 100),
+				new Line3D(100, 100, 100, 100, -100, 100),
+
+				new Line3D(-100, -100, -100, -100, -100, 100),
+				new Line3D(100, -100, -100, 100, -100, 100),
+				new Line3D(-100, 100, -100, -100, 100, 100),
+				new Line3D(100, 100, -100, 100, 100, 100),
+			});
+
+			AddLines();
+		}
+
+		private void HumanMenuItem_Click(object sender, RoutedEventArgs e) {
+			HumanMenuItem.IsChecked = true;
+			CubeMenuItem.IsChecked = PiramideMenuItem.IsChecked = false;
+
+			ClearLines();
+			var body = new Cube3D(-40, -50, -25, 40, 70, 25);
+			lines3D.AddRange(body.GetLines());
+
+			var head = new Cube3D(-20, -50, -25, 20, -90, 25);
+			lines3D.AddRange(head.GetLines());
+
+			var leye = new Cube3D(-15, -80, -25, -2, -75, -25);
+			lines3D.AddRange(leye.GetLines());
+
+			var reye = new Cube3D(2, -80, -25, 15, -75, -25);
+			lines3D.AddRange(reye.GetLines());
+
+			var larm = new Cube3D(40, -50, -12, 60, 40, 12);
+			lines3D.AddRange(larm.GetLines());
+
+			var rarm = new Cube3D(-40, -50, -12, -60, 40, 12);
+			lines3D.AddRange(rarm.GetLines());
+
+			var lleg = new Cube3D(-30, 70, -14, -10, 170, 14);
+			lines3D.AddRange(lleg.GetLines());
+
+			var rleg = new Cube3D(10, 70, -14, 30, 170, 14);
+			lines3D.AddRange(rleg.GetLines());
+
+			AddLines();
 		}
 	}
 }
