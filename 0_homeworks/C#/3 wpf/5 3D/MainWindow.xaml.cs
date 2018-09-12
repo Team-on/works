698536a8181aebdf9fbbe5c8ea39ff0d0f804554
Team-on <timonsol13@gmail.com>
2018-09-12 20:46:21 +0300

    Like3D angle

--- 0_homeworks/C#/3 wpf/5 3D/MainWindow.xaml.cs
+++ 0_homeworks/C#/3 wpf/5 3D/MainWindow.xaml.cs
@@ -47,6 +47,9 @@ namespace Like3D {
 		private void Window_KeyDown(object sender, KeyEventArgs e) {
 			Point3D newPos = camera.Position;
 			Vector3D upDir = camera.UpDirection;
+
+			//double angle = Math.Atan2(y2 - y1, x2 - x1) - Math.Atan2(y4 - y3, x4 - x3);
+
 			switch(e.Key) {
 			case Key.Escape:
 			this.Close();
