c21ab80c71d7bed12c647fd1c18bcd46f20a56e7
Team-on <timonsol13@gmail.com>
2018-09-12 19:30:50 +0300

    Init task manager

---
+++ 2_prj/[C#] TaskManager/source/TaskManager/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace TaskManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.listbox = new System.Windows.Forms.ListBox();
+			this.SuspendLayout();
+			// 
+			// listbox
+			// 
+			this.listbox.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.listbox.FormattingEnabled = true;
+			this.listbox.Location = new System.Drawing.Point(0, 0);
+			this.listbox.Name = "listbox";
+			this.listbox.Size = new System.Drawing.Size(284, 262);
+			this.listbox.TabIndex = 0;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 262);
+			this.Controls.Add(this.listbox);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+
+        }
+
+		#endregion
+
+		private System.Windows.Forms.ListBox listbox;
+	}
+}
+
---
+++ 2_prj/[C#] TaskManager/source/TaskManager/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaskManager {
+	public partial class Form1 : Form {
+		List<Process> process;
+
+		public Form1() {
+			process = new List<Process>();
+
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e) {
+			
+		}
+	}
+}
