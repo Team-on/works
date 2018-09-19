85e5aad988a054a0937766f8e072710c597db417
Team-on <timonsol13@gmail.com>
2018-09-19 15:33:14 +0300

    [MonaGame] test windows and android

---
+++ 0_homeworks/C#/7 monogame/1/WindownMain/Game1.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace _1 {
+	/// <summary>
+	/// This is the main type for your game.
+	/// </summary>
+	public class Game1 : Game {
+		GraphicsDeviceManager graphics;
+		SpriteBatch spriteBatch;
+		Texture2D arthas;
+		Effect shader;
+
+		public Game1() {
+			graphics = new GraphicsDeviceManager(this);
+			Content.RootDirectory = "Content";
+
+#if ANDROID
+			graphics.IsFullScreen = true;
+#endif
+			graphics.PreferredBackBufferWidth = 800;
+			graphics.PreferredBackBufferHeight = 480;
+			graphics.SupportedOrientations = DisplayOrientation.LandscapeLeft | DisplayOrientation.LandscapeRight;
+		}
+
+		/// <summary>
+		/// Allows the game to perform any initialization it needs to before starting to run.
+		/// This is where it can query for any required services and load any non-graphic
+		/// related content.  Calling base.Initialize will enumerate through any components
+		/// and initialize them as well.
+		/// </summary>
+		protected override void Initialize() {
+			// TODO: Add your initialization logic here
+
+			base.Initialize();
+		}
+
+		/// <summary>
+		/// LoadContent will be called once per game and is the place to load
+		/// all of your content.
+		/// </summary>
+		protected override void LoadContent() {
+			// Create a new SpriteBatch, which can be used to draw textures.
+			spriteBatch = new SpriteBatch(GraphicsDevice);
+
+			// TODO: use this.Content to load your game content here
+			arthas = Content.Load<Texture2D>("Arthas");
+			shader = Content.Load<Effect>("shader");
+		}
+
+		/// <summary>
+		/// UnloadContent will be called once per game and is the place to unload
+		/// game-specific content.
+		/// </summary>
+		protected override void UnloadContent() {
+			// TODO: Unload any non ContentManager content here
+		}
+
+		/// <summary>
+		/// Allows the game to run logic such as updating the world,
+		/// checking for collisions, gathering input, and playing audio.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Update(GameTime gameTime) {
+			if(GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+				Exit();
+
+			// TODO: Add your update logic here
+
+			base.Update(gameTime);
+		}
+
+		/// <summary>
+		/// This is called when the game should draw itself.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Draw(GameTime gameTime) {
+			GraphicsDevice.Clear(Color.CornflowerBlue);
+
+			// TODO: Add your drawing code here
+			spriteBatch.Begin(SpriteSortMode.Immediate, null, null, null, null, shader, null);
+			spriteBatch.Draw(arthas, new Rectangle(0, 0, graphics.PreferredBackBufferWidth, graphics.PreferredBackBufferHeight), Color.White);
+			spriteBatch.End();
+
+			base.Draw(gameTime);
+		}
+	}
+}
