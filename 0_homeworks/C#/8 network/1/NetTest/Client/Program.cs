f72009422ef0b78c519ec6cc36c334a381208bb9
Team-on <timonsol13@gmail.com>
2018-10-01 20:24:17 +0300

    [PicsDownloader] Init

--- 0_homeworks/C#/8 network/1/NetTest/Client/Program.cs
+++ 0_homeworks/C#/8 network/1/NetTest/Client/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NetTest {
+namespace Client {
 	class Program {
 		static void Main(string[] args) {
 		}
--- 0_homeworks/C#/8 network/1/NetTest/Client/Properties/AssemblyInfo.cs
+++ 0_homeworks/C#/8 network/1/NetTest/Client/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NetTest")]
+[assembly: AssemblyTitle("Client")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("NetTest")]
+[assembly: AssemblyProduct("Client")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a6280df8-d073-4813-9519-34034b7bb6ee")]
+[assembly: Guid("46759c4d-845f-44f0-b1c7-34e0371004dd")]
 
 // Version information for an assembly consists of the following four values:
 //
