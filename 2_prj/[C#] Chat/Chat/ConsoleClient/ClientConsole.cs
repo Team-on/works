2bdd5d142cb9c0ba489b0b9fed5c7e49a2bbee37
Team-on <timonsol13@gmail.com>
2018-10-05 20:07:38 +0300

    [Chat] New client input

--- 2_prj/[C#] Chat/Chat/ConsoleClient/ClientConsole.cs
+++ 2_prj/[C#] Chat/Chat/ConsoleClient/ClientConsole.cs
@@ -11,28 +11,47 @@ namespace ConsoleClient {
 		static bool isRunning = true;
 
 		static void Main(string[] args) {
-			User user = new User("Console user", ConsoleColor.White);
+			Console.Title = "Chat";
+
+			User user = new User("ConsoleUser", ConsoleColor.White);
 
 			Client client = new Client();
 			Console.CancelKeyPress += (a, b) => client.Dispose();
 			client.SetUser(user);
-			client.SetConnection("127.0.0.1", 63255);
+			//client.SetConnection("127.0.0.1", 63255);
 
-			client.Send("Hi, its me - " + user.Name);
+			//client.Send("Hi, its me - " + user.Name);
 
 			string message = "";
+
 			while (isRunning) {
-				if (client.IsRecieve())
-					Console.WriteLine(client.Recieve());
+				if(Console.CursorLeft != 9 && message == "") {
+					Console.SetCursorPosition(0, 0);
+					Console.SetCursorPosition(0, Console.WindowHeight - 1);
+					Console.Write("Message: ");
+				}
+
+				//if(client.IsRecieve()) {
+				//	Console.WriteLine(client.Recieve());
+				//}
+
 				if (Console.KeyAvailable) {
+					Console.CursorVisible = false;
 					ConsoleKeyInfo key = Console.ReadKey();
-					if (key.Key == ConsoleKey.Escape)
+					Console.CursorVisible = true;
+
+					if(key.Key == ConsoleKey.Escape) {
 						isRunning = false;
-					else if(key.Key != ConsoleKey.Enter){
+					}
+					else if(key.Key == ConsoleKey.Enter) {
+						//client.Send(message);
+						message = "";
+						Console.SetCursorPosition(0, Console.WindowHeight - 1);
+						Console.Write(new string(' ', Console.WindowWidth));
+					}
+					else {
 						message += key.KeyChar;
 					}
-					else
-						client.Send(message);
 				}
 			}
 
