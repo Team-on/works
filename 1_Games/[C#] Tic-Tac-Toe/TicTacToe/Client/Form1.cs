9318942d3c96f20dbe23accf5f23233b0b9ff4d9
Team-on <timonsol13@gmail.com>
2018-10-13 01:45:06 +0300

    [Tic-Tac-Toe] Fix client code

--- 1_Games/[C#] Tic-Tac-Toe/TicTacToe/Client/Form1.cs
+++ 1_Games/[C#] Tic-Tac-Toe/TicTacToe/Client/Form1.cs
@@ -15,63 +15,83 @@ namespace Client {
 		TcpClient tcp = new TcpClient();
 		NetworkStream stream;
 		Thread r;
-		byte[] one = new byte[1];
 
 		public Form1() {
 			InitializeComponent();
 			r = new Thread(new ThreadStart(recieve));
 			r.Start();
 		}
+
 		public void recieve() {
-			while(true)
-				if(stream != null) {
-					while(true) {
-						byte[] arr = new byte[1];
-						stream.Read(arr, 0, 1);
-						if(arr[0] == 1) {
-							arr = new byte[3];
-							stream.Read(arr, 0, arr.Length);
-							foreach(var item in this.Controls) {
-								if(item != null && (((item as Button)?.Tag as String)?.Equals(arr[0].ToString()) ?? false)) {
-									(item as Button).Text = arr[1] == 1 ? "X" : "O";
-									switch(arr[2]) {
-										case 1: MessageBox.Show("Nichiya"); break;
-										case 2: MessageBox.Show("Win : O"); break;
-										case 3: MessageBox.Show("Win : X"); break;
-									}
-									if(arr[2] > 0 && arr[2] < 4) {
-										stream.Close();
-										tcp.Close();
-									}
-								}
+			byte[] arr = new byte[3];
+			int readRez = 0;
+
+			while(true) {
+				readRez = stream?.ReadByte() ?? -1;
+
+				if(readRez == -1) {
+					Thread.Sleep(10);
+					continue;
+				}
+
+				else if(readRez == 1) {
+					stream.Read(arr, 0, 3);
+					MessageBox.Show(string.Format($"\tRecieve: {arr[0]} {arr[1]} {arr[2]}"));
+
+					foreach(var item in this.Controls) {
+						if(item != null && (((item as Button)?.Tag as string)?.Equals(arr[0].ToString()) ?? false)) {
+							(item as Button).Text = arr[1] == 0 ? "0" : "X";
+
+							//MessageBox.Show(arr[0].ToString() + "  |  " + ((item as Button)?.Tag as string));
+
+							switch(arr[2]) {
+								case 1: MessageBox.Show("Nichiya"); break;
+								case 2: MessageBox.Show("Win: O"); break;
+								case 3: MessageBox.Show("Win: X"); break;
 							}
+
+							if(arr[2] != 0) {
+								stream.Close();
+								tcp.Close();
+								return;
+							}
+
+							break;
 						}
 					}
 				}
+
+			}
 		}
+
 		private void button10_Click(object sender, EventArgs e) {
-			Task a = new Task(new Action(delegate () {
-				try {
-					tcp.Connect(textBox1.Text, Int32.Parse(textBox2.Text));
-					stream = tcp.GetStream();
-					stream.Read(one, 0, 1);
-					textBox1.Enabled = textBox2.Enabled = false;
-					label2.Text = one[0] == 1 ? "0" : "Y";
-				}
-				catch(Exception s) { MessageBox.Show(s.Message); }
-			}));
-			a.Start();
+			new Task(
+				new Action(delegate () {
+					try {
+						tcp.Connect(textBox1.Text, Int32.Parse(textBox2.Text));
+						stream = tcp.GetStream();
+						button10.Enabled = textBox1.Enabled = textBox2.Enabled = false;
+						byte rez = (byte)stream.ReadByte();
+						label2.Text = rez == 0 ? "0" : "X";
+					}
+					catch(Exception s) {
+						MessageBox.Show(s.Message);
+					}
+				})
+			).Start();
 		}
+
 		private void CLICK(object sender, EventArgs e) {
-			Task l = new Task(new Action(delegate () { stream.Write(BitConverter.GetBytes(Int32.Parse((sender as Button).Name[1].ToString())), 0, 1); }));
-			l.Start();
+			new Task(
+				new Action(delegate () {
+					stream.WriteByte(byte.Parse(((sender as Button).Tag as string)));
+				})
+			).Start();
 		}
 
 		private void Form1_FormClosing(object sender, FormClosingEventArgs e) {
-			if(stream != null)
-				stream.Close();
-			if(tcp != null)
-				tcp.Close();
+			stream?.Close();
+			tcp?.Close();
 		}
 	}
 }
