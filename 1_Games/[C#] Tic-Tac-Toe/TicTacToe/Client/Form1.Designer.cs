7b72986b94f05fea429764deb025bb50fac1710f
Team-on <timonsol13@gmail.com>
2018-10-13 01:05:00 +0300

    [Tic-Tac-Toe] Add client

---
+++ 1_Games/[C#] Tic-Tac-Toe/TicTacToe/Client/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace Client {
+	partial class Form1 {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if(disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this._0 = new System.Windows.Forms.Button();
+			this._1 = new System.Windows.Forms.Button();
+			this._2 = new System.Windows.Forms.Button();
+			this._3 = new System.Windows.Forms.Button();
+			this._4 = new System.Windows.Forms.Button();
+			this._5 = new System.Windows.Forms.Button();
+			this._6 = new System.Windows.Forms.Button();
+			this._7 = new System.Windows.Forms.Button();
+			this._8 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.button10 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// _0
+			// 
+			this._0.Location = new System.Drawing.Point(0, 0);
+			this._0.Name = "_0";
+			this._0.Size = new System.Drawing.Size(75, 75);
+			this._0.TabIndex = 0;
+			this._0.Tag = "0";
+			this._0.UseVisualStyleBackColor = true;
+			this._0.Click += new System.EventHandler(this.CLICK);
+			// 
+			// _1
+			// 
+			this._1.Location = new System.Drawing.Point(81, 0);
+			this._1.Name = "_1";
+			this._1.Size = new System.Drawing.Size(75, 75);
+			this._1.TabIndex = 1;
+			this._1.Tag = "1";
+			this._1.UseVisualStyleBackColor = true;
+			this._1.Click += new System.EventHandler(this.CLICK);
+			// 
+			// _2
+			// 
+			this._2.Location = new System.Drawing.Point(162, 0);
+			this._2.Name = "_2";
+			this._2.Size = new System.Drawing.Size(75, 75);
+			this._2.TabIndex = 2;
+			this._2.Tag = "2";
+			this._2.UseVisualStyleBackColor = true;
+			this._2.Click += new System.EventHandler(this.CLICK);
+			// 
+			// _3
+			// 
+			this._3.Location = new System.Drawing.Point(0, 81);
+			this._3.Name = "_3";
+			this._3.Size = new System.Drawing.Size(75, 75);
+			this._3.TabIndex = 3;
+			this._3.Tag = "3";
+			this._3.UseVisualStyleBackColor = true;
+			this._3.Click += new System.EventHandler(this.CLICK);
+			// 
+			// _4
+			// 
+			this._4.Location = new System.Drawing.Point(81, 81);
+			this._4.Name = "_4";
+			this._4.Size = new System.Drawing.Size(75, 75);
+			this._4.TabIndex = 4;
+			this._4.Tag = "4";
+			this._4.UseVisualStyleBackColor = true;
+			this._4.Click += new System.EventHandler(this.CLICK);
+			// 
+			// _5
+			// 
+			this._5.Location = new System.Drawing.Point(162, 81);
+			this._5.Name = "_5";
+			this._5.Size = new System.Drawing.Size(75, 75);
+			this._5.TabIndex = 5;
+			this._5.Tag = "5";
+			this._5.UseVisualStyleBackColor = true;
+			this._5.Click += new System.EventHandler(this.CLICK);
+			// 
+			// _6
+			// 
+			this._6.Location = new System.Drawing.Point(0, 162);
+			this._6.Name = "_6";
+			this._6.Size = new System.Drawing.Size(75, 75);
+			this._6.TabIndex = 6;
+			this._6.Tag = "6";
+			this._6.UseVisualStyleBackColor = true;
+			this._6.Click += new System.EventHandler(this.CLICK);
+			// 
+			// _7
+			// 
+			this._7.Location = new System.Drawing.Point(81, 162);
+			this._7.Name = "_7";
+			this._7.Size = new System.Drawing.Size(75, 75);
+			this._7.TabIndex = 7;
+			this._7.Tag = "7";
+			this._7.UseVisualStyleBackColor = true;
+			this._7.Click += new System.EventHandler(this.CLICK);
+			// 
+			// _8
+			// 
+			this._8.Location = new System.Drawing.Point(162, 162);
+			this._8.Name = "_8";
+			this._8.Size = new System.Drawing.Size(75, 75);
+			this._8.TabIndex = 8;
+			this._8.Tag = "8";
+			this._8.UseVisualStyleBackColor = true;
+			this._8.Click += new System.EventHandler(this.CLICK);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(243, 112);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(41, 13);
+			this.label1.TabIndex = 9;
+			this.label1.Text = "Знак : ";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(281, 114);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(13, 13);
+			this.label2.TabIndex = 10;
+			this.label2.Text = "0";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(12, 249);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(26, 13);
+			this.label3.TabIndex = 11;
+			this.label3.Text = "IP : ";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(35, 246);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(202, 20);
+			this.textBox1.TabIndex = 12;
+			this.textBox1.Text = "127.0.0.1";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(3, 273);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(35, 13);
+			this.label4.TabIndex = 13;
+			this.label4.Text = "Port : ";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(35, 272);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(202, 20);
+			this.textBox2.TabIndex = 14;
+			this.textBox2.Text = "63100";
+			// 
+			// button10
+			// 
+			this.button10.Location = new System.Drawing.Point(243, 246);
+			this.button10.Name = "button10";
+			this.button10.Size = new System.Drawing.Size(59, 46);
+			this.button10.TabIndex = 15;
+			this.button10.Text = "Connect";
+			this.button10.UseVisualStyleBackColor = true;
+			this.button10.Click += new System.EventHandler(this.button10_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(312, 310);
+			this.Controls.Add(this.button10);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this._8);
+			this.Controls.Add(this._7);
+			this.Controls.Add(this._6);
+			this.Controls.Add(this._5);
+			this.Controls.Add(this._4);
+			this.Controls.Add(this._3);
+			this.Controls.Add(this._2);
+			this.Controls.Add(this._1);
+			this.Controls.Add(this._0);
+			this.MaximumSize = new System.Drawing.Size(320, 337);
+			this.MinimumSize = new System.Drawing.Size(320, 337);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button _0;
+		private System.Windows.Forms.Button _1;
+		private System.Windows.Forms.Button _2;
+		private System.Windows.Forms.Button _3;
+		private System.Windows.Forms.Button _4;
+		private System.Windows.Forms.Button _5;
+		private System.Windows.Forms.Button _6;
+		private System.Windows.Forms.Button _7;
+		private System.Windows.Forms.Button _8;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Button button10;
+	}
+}
+
---
+++ 1_Games/[C#] Tic-Tac-Toe/TicTacToe/Client/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Client {
+	public partial class Form1 : Form {
+		TcpClient tcp = new TcpClient();
+		NetworkStream stream;
+		Thread r;
+		byte[] one = new byte[1];
+
+		public Form1() {
+			InitializeComponent();
+			r = new Thread(new ThreadStart(recieve));
+			r.Start();
+		}
+		public void recieve() {
+			while(true)
+				if(stream != null) {
+					while(true) {
+						byte[] arr = new byte[1];
+						stream.Read(arr, 0, 1);
+						if(arr[0] == 1) {
+							arr = new byte[3];
+							stream.Read(arr, 0, arr.Length);
+							foreach(var item in this.Controls) {
+								if(item != null && (((item as Button)?.Tag as String)?.Equals(arr[0].ToString()) ?? false)) {
+									(item as Button).Text = arr[1] == 1 ? "X" : "O";
+									switch(arr[2]) {
+										case 1: MessageBox.Show("Nichiya"); break;
+										case 2: MessageBox.Show("Win : O"); break;
+										case 3: MessageBox.Show("Win : X"); break;
+									}
+									if(arr[2] > 0 && arr[2] < 4) {
+										stream.Close();
+										tcp.Close();
+									}
+								}
+							}
+						}
+					}
+				}
+		}
+		private void button10_Click(object sender, EventArgs e) {
+			Task a = new Task(new Action(delegate () {
+				try {
+					tcp.Connect(textBox1.Text, Int32.Parse(textBox2.Text));
+					stream = tcp.GetStream();
+					stream.Read(one, 0, 1);
+					textBox1.Enabled = textBox2.Enabled = false;
+					label2.Text = one[0] == 1 ? "0" : "Y";
+				}
+				catch(Exception s) { MessageBox.Show(s.Message); }
+			}));
+			a.Start();
+		}
+		private void CLICK(object sender, EventArgs e) {
+			Task l = new Task(new Action(delegate () { stream.Write(BitConverter.GetBytes(Int32.Parse((sender as Button).Name[1].ToString())), 0, 1); }));
+			l.Start();
+		}
+
+		private void Form1_FormClosing(object sender, FormClosingEventArgs e) {
+			if(stream != null)
+				stream.Close();
+			if(tcp != null)
+				tcp.Close();
+		}
+	}
+}
