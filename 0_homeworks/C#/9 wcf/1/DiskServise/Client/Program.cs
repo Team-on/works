d8d36a05a809ffc9f01e5bf17bc6719835e6b5bf
Team-on <timonsol13@gmail.com>
2018-10-17 19:31:34 +0300

    [DiskInfo] Add mex

--- 0_homeworks/C#/9 wcf/1/DiskServise/Client/Program.cs
+++ 0_homeworks/C#/9 wcf/1/DiskServise/Client/Program.cs
@@ -2,30 +2,24 @@
 using System.IO;
 
 using System.ServiceModel;
-using DiskServise;
 
 namespace Client {
 	class Program {
 		static void Main(string[] args) {
-			ChannelFactory<IDiskServise> factory = new ChannelFactory<IDiskServise>(new BasicHttpBinding(),
-				new EndpointAddress("http://localhost:8733/Design_Time_Addresses/DiskServiseDll/DiskServise/")
-			);
+			using (DiskServiseLink.DiskServiseClient cl = new DiskServiseLink.DiskServiseClient()) {
+				Console.Write("Input path: ");
+				string[] result = cl.GetDiskInfo(Console.ReadLine());
 
-			IDiskServise channel = factory.CreateChannel();
+				foreach (var i in result) {
+					if (Path.GetExtension(i) == "")
+						Console.Write("Directory\t");
+					else
+						Console.Write("File\t");
+					Console.WriteLine(i);
+				}
 
-			Console.Write("Input path: ");
-			string[] result = channel.GetDiskInfo(Console.ReadLine());
-
-			foreach (var i in result) {
-				if(Path.GetExtension(i) == "")
-					Console.Write("Directory\t");
-				else
-					Console.Write("File\t");
-				Console.WriteLine(i);
 			}
-
 			Console.ReadLine();
-			factory.Close();
 		}
 	}
 }
