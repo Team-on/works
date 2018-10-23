591c9e41ed15635222f33de76435449fc79c9665
Team-on <timonsol13@gmail.com>
2018-10-23 19:38:45 +0300

    [WCF] Done homework 3

---
+++ 0_homeworks/C#/9 wcf/3/DualHttp/Client/ClientCallback.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client {
+	class ClientCallback : ServiceReference1.IService1Callback {
+		public Client.Form1 form;
+
+		public void TextForUsers(string txt) {
+			form.textBox3.Text = txt;
+		}
+	}
+}
