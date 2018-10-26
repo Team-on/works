e685af3e0e2775a926f9cc9a948a77398133a08c
Team <timonsol13@gmail.com>
2018-10-26 18:04:42 +0300

    [TAO] Init test

---
+++ 0_homeworks/C#/TAO Framework/1/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace TaoTest
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.simpleOpenGlControl1 = new Tao.Platform.Windows.SimpleOpenGlControl();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// simpleOpenGlControl1
+			// 
+			this.simpleOpenGlControl1.AccumBits = ((byte)(0));
+			this.simpleOpenGlControl1.AutoCheckErrors = false;
+			this.simpleOpenGlControl1.AutoFinish = false;
+			this.simpleOpenGlControl1.AutoMakeCurrent = true;
+			this.simpleOpenGlControl1.AutoSwapBuffers = true;
+			this.simpleOpenGlControl1.BackColor = System.Drawing.Color.Black;
+			this.simpleOpenGlControl1.ColorBits = ((byte)(32));
+			this.simpleOpenGlControl1.DepthBits = ((byte)(16));
+			this.simpleOpenGlControl1.Location = new System.Drawing.Point(12, 12);
+			this.simpleOpenGlControl1.Name = "simpleOpenGlControl1";
+			this.simpleOpenGlControl1.Size = new System.Drawing.Size(647, 604);
+			this.simpleOpenGlControl1.StencilBits = ((byte)(0));
+			this.simpleOpenGlControl1.TabIndex = 0;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(683, 44);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Show";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(683, 84);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "Exit";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(819, 628);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.simpleOpenGlControl1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private SimpleOpenGlControl simpleOpenGlControl1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+	}
+}
+
---
+++ 0_homeworks/C#/TAO Framework/1/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Tao.FreeGlut;
+using Tao.OpenGl;
+using Tao.Platform.Windows;
+
+namespace TaoTest {
+	public partial class Form1 : Form {
+		public Form1() {
+			InitializeComponent();
+		}
+	}
+}
