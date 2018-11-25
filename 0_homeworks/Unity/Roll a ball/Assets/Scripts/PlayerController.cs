b2404209813ebc8e1cd18c88ce3ae171acb5585c
Team <timonsol13@gmail.com>
2018-11-25 19:01:31 +0200

    [Roll a ball] Move to homeworks

---
+++ 0_homeworks/Unity/Roll a ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+	Rigidbody rb;
+
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis("Horizontal"),
+			  moveVertical = Input.GetAxis("Vertical");
+		Vector3 movement = new Vector3(moveHorizontal * speed, 0, moveVertical * speed);
+			
+		rb.AddForce(movement);
+	}
+
+
+}
