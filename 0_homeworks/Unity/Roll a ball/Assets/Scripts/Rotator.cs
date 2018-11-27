7b5e55b7ea3d1fc7825f2fb2db37c5b2a8c5aa87
Team <timonsol13@gmail.com>
2018-11-27 21:34:59 +0200

    [Roll a ball] Remove prj

--- 0_homeworks/Unity/Roll a ball/Assets/Scripts/Rotator.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Rotator : MonoBehaviour {
-	void Update () {
-		transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
-	}
-}
