a2d26195673b898953c86174cdf3b490f57f3533
Team <timonsol13@gmail.com>
2018-11-27 21:44:04 +0200

    [Roll a ball] Reload

---
+++ 0_homeworks/Unity/Roll a ball/Assets/Scripts/Rotator.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+	void Update () {
+		transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+	}
+}
