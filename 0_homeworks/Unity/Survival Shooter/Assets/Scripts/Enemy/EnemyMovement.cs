2510b550dac6b8da5119e2a482771fd240db7f2d
Team <timonsol13@gmail.com>
2019-01-14 17:32:59 +0200

    [Survival Shooter] Creating Enemy #1

---
+++ 0_homeworks/Unity/Survival Shooter/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+	Transform player;
+	UnityEngine.AI.NavMeshAgent nav;
+
+	void Awake() {
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+		nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
+	}
+
+
+	void Update() {
+		nav.SetDestination(player.position);
+	}
+}
