d10d4562bbb6bb7a385a43605dc4c9d02c426bc8
Team <timonsol13@gmail.com>
2019-01-23 01:08:26 +0200

    [Survival Shooter] Spawning Enemies

--- 0_homeworks/Unity/Survival Shooter/Assets/Scripts/Manager/EnemyManager.cs
+++ 0_homeworks/Unity/Survival Shooter/Assets/Scripts/Manager/EnemyManager.cs
@@ -1,16 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class EnemyManager : MonoBehaviour {
+	public PlayerHealth playerHealth;       // Reference to the player's heatlh.
+	public float spawnTime = 3f;            // How long between each spawn.
+	public GameObject[] enemies;                // The enemy prefab to be spawned.
+	public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
 
-	// Use this for initialization
-	void Start () {
-		
+
+	void Start() {
+		InvokeRepeating("Spawn", spawnTime, spawnTime);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+
+	void Spawn() {
+		if (playerHealth.currentHealth <= 0)
+			return;
+
+		int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+		int enamyIndex = Random.Range(0, enemies.Length);
+		Instantiate(enemies[enamyIndex], spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
 	}
 }
