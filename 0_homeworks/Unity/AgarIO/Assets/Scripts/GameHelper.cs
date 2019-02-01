8c9f6b0e6843bc79261bcb9d18a14cc383cb9ac0
Team <timonsol13@gmail.com>
2019-02-01 15:33:23 +0200

    [AgarIO] Moving & spawn

---
+++ 0_homeworks/Unity/AgarIO/Assets/Scripts/GameHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GameHelper : NetworkBehaviour {
+	[Server]
+	void Start () {
+		for (int i = 0; i < 10; i++) {
+			print("spawn point");
+			//GetComponent<NetworkManager>().spawnPrefabs[0]
+			GameObject point = Instantiate(Resources.Load<GameObject>("Point"));
+			point.transform.position = Random.insideUnitCircle * Random.Range(10, 30);
+			NetworkServer.Spawn(point);
+		}
+	}
+	
+	void Update () {
+		
+	}
+}
---
+++ 0_homeworks/Unity/AgarIO/Assets/Scripts/PlayerHelper.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerHelper : NetworkBehaviour {
+	public const float speedMod = 1f;
+	
+	[SyncVar]
+	public float size = 0.2f;
+
+	[SyncVar]
+	float speed;
+
+	void Start () {
+		ChangeSize(size);
+	}
+	
+	void Update () {
+
+		if (!isLocalPlayer)
+			return;
+
+		Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		transform.position = Vector3.MoveTowards(transform.position, mousePos, speed * Time.deltaTime);
+	}
+
+	[Server]
+	void ChangeSize(float newSize){
+		size = newSize;
+		speed = speedMod / size;
+		transform.localScale = new Vector3(size, size, size);
+	}
+}
