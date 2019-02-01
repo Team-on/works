940259db24019713f7066a14b9112f1aec780db0
Team <timonsol13@gmail.com>
2019-02-01 03:42:44 +0200

    [UChat] Done

---
+++ 0_homeworks/Unity/Chat/Assets/Scripts/GameHelper.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameHelper : MonoBehaviour {
+	public InputField input;
+	public Text text;
+
+	internal PlayerHelper localPlayerHelper;
+
+	void Start() {
+	}
+
+
+	void Update() {
+
+	}
+
+	public void Send(){
+		if (localPlayerHelper != null)
+		localPlayerHelper.Send(input.text);
+	}
+}
---
+++ 0_homeworks/Unity/Chat/Assets/Scripts/PlayerHelper.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerHelper : NetworkBehaviour {
+	GameHelper localGameHelper;
+	NetworkIdentity ni;
+
+	void Start() {
+		ni = GetComponent<NetworkIdentity>();
+		localGameHelper = FindObjectOfType<GameHelper>();
+
+		if (isLocalPlayer)
+			localGameHelper.localPlayerHelper = this;
+	}
+
+	public void Send(string message) {
+		CmdSend(ni.netId.Value.ToString(), message);
+	}
+
+	[Command]
+	void CmdSend(string id, string message) {
+		RpcSend(id, message);
+		
+		if (message == "/random")
+			RpcSend("Server", Random.Range(0, 21).ToString());
+	}
+
+	[ClientRpc]
+	void RpcSend(string id, string message) {
+		localGameHelper.text.text += id + ": " + message + System.Environment.NewLine;
+	}
+
+	void Update() {
+
+	}
+}
