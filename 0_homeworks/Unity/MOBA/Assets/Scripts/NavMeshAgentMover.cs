ec0f036261ca58d41ec98086a51487d401f783ef
Team <timonsol13@gmail.com>
2019-02-10 19:24:34 +0200

    [MOBA] Player move

---
+++ 0_homeworks/Unity/MOBA/Assets/Scripts/NavMeshAgentMover.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class NavMeshAgentMover : NetworkBehaviour {
+	[Header("NetworkSettings")]
+	public float SyncRate = 3;
+
+	[SyncVar]
+	Vector3 position;
+	[SyncVar]
+	Quaternion rotation;
+
+	Animator animator;
+	NavMeshAgent navMeshAgent;
+	Vector3 target;
+
+	void Start () {
+		navMeshAgent = GetComponent<NavMeshAgent>();
+		animator = GetComponent<Animator>();
+		target = transform.position;
+	}
+	
+	[ServerCallback]
+	void Update () {
+		Animation();
+
+		if (position == transform.position && rotation == transform.rotation)
+			return;
+
+		position = transform.position;
+		rotation = transform.rotation;
+	}
+
+	[ClientCallback]
+	void FixedUpdate(){
+		if(transform.position != position)
+			transform.position = Vector3.Lerp(transform.position, position, Time.deltaTime * SyncRate);
+		if(transform.rotation != rotation)
+			transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * SyncRate);
+	}
+
+	public void Move(Vector3 point){
+		CmdMove(point);
+	}
+
+	[Command]
+	public void CmdMove(Vector3 point) {
+		target = point;
+		navMeshAgent.SetDestination(target);
+	}
+
+	void Animation(){
+
+	}
+}
---
+++ 0_homeworks/Unity/MOBA/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+[RequireComponent(typeof(NavMeshAgentMover))]
+public class PlayerInput : NetworkBehaviour {
+	NavMeshAgentMover navMeshAgentMover;
+
+	void Start(){
+		if (!isLocalPlayer){ 
+			enabled = false;
+			return;
+		}
+
+		navMeshAgentMover = GetComponent<NavMeshAgentMover>();
+	}
+
+	void Update() {
+		if(Input.GetMouseButtonDown(0)){
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if(Physics.Raycast(ray, out hit) && hit.transform.tag == "Ground")
+				navMeshAgentMover.Move(hit.point);
+		}
+	}
+}
